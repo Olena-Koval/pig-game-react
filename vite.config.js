b45282aa3b564d31ab0cd2a5ecf547edca9e3eac
Olena-Koval <19111982e@gmail.com>
2025-02-20 11:04:45 +0100

    Mensaje descriptivo de los cambios realizados

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: 'https://olena-koval.github.io/pig-game-react/', // Asegúrate de que esta sea la ruta correcta de tu repositorio en GitHub
 })
